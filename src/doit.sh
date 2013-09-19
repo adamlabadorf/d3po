cb839c8cd6b4b6d4bb73d1dc6fe2336915473708
Adam Labadorf <alabadorf@gmail.com>
2013-09-19 19:27:45 -0400

    added convenience script

---
+++ src/doit.sh
@@ -0,0 +1,1 @@
+git add d3pyo.py d3po.js d3po.ipynb; gici -m 'updated'; git push; pushd; cd src/d3po; git pull; cd ../..; . stage.sh; git add src/d3po lib/d3po.js; gici -m 'updated'; git push; pushd
