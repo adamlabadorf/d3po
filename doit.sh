4baad0d0044b95526a2d429420fc6cdb21200b92
Adam Labadorf <alabadorf@gmail.com>
2013-10-09 11:46:09 -0400

    cleaned up old files

--- doit.sh
+++
@@ -1,1 +0,0 @@
-git add d3pyo.py d3po.js d3po.ipynb; gici -m 'updated'; git push; pushd; cd src/d3po; git pull; cd ../..; . stage.sh; git add src/d3po lib/d3po.js; gici -m 'updated'; git push; pushd
